dc2c07a5f3b6674d57c4f8b0001c5a97ae4693d1
Mateusz Wawrzyniak <zostawciekawosc@gmail.com>
2023-09-08 21:12:33 +0200

    Open source map added to application.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import "../../utils/fix-map-icon";
 
-import './Map.css'
+import 'leaflet/dist/leaflet.css';
+import './Map.css';
 
 export const Map = ()=> {
     return (
-        <div className="map"/>
+        <div className="map">
+            <MapContainer center={[53.415242,14.5351685]} zoom={15}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution= "&copy; <a href='https://openstreetmap.org/copyright'>OpenStreetMap contributors</a>"
+                />
+                <Marker position={[53.415242,14.5351685]}>
+                    <Popup>
+                        <h2>Super miejsce</h2>
+                        <p>Najlepsza ośka</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     );
 };
