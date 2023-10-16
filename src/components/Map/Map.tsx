21ba92918617448b11fe6175ee3fe4e2fae0475c
Mateusz Wawrzyniak <mateusz.wawrzyniak3@gmail.com>
2023-10-16 15:01:41 +0200

    Map and markers are finished.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,34 +1,46 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import "../../utils/fix-map-icon";
-
 
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
 import {SearchContext} from "../../contexts/search.context";
+import {SimpleAdEntity} from 'types';
+import {SingleAd} from "./SingleAd";
 
 export const Map = ()=> {
 
     const {search} = useContext(SearchContext);
+    const [ads, setAds] = useState<SimpleAdEntity[]>([]);
 
-    useEffect(()=> {
-        console.log('Make request to search for', search);
+
+    useEffect(() => {
+        (async ()=> {
+
+            const res = await fetch(`http://localhost:3001/ad/search/${search}`);
+            const ads = await res.json();
+            setAds(ads);
+
+        })();
     }, [search]);
 
     return (
         <div className="map">
-            <h1>Search for: {search}</h1>
             <MapContainer center={[53.415242,14.5351685]} zoom={15}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution= "&copy; <a href='https://openstreetmap.org/copyright'>OpenStreetMap contributors</a>"
                 />
-                <Marker position={[53.415242,14.5351685]}>
-                    <Popup>
-                        <h2>Super miejsce</h2>
-                        <p>Najlepsza ośka</p>
-                    </Popup>
-                </Marker>
+                {
+                    ads.map(ad => (
+                        <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                            <Popup>
+                                <SingleAd id={ad.id}/>
+                            </Popup>
+                        </Marker>
+                    ))
+                }
+
             </MapContainer>
         </div>
     );
